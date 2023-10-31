47af272594536dabc0d552841ac08df52ae694cb
seongssu <loveforggong@gmail.com>
2023-10-31 16:56:53 +0900

    test

---
+++ app/src/main/java/kr/sparta/test/MainActivity.kt
@@ -0,0 +1,18 @@
+package kr.sparta.test
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.sparta.test.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnSharedpreference.setOnClickListener {
+
+        }
+    }
+}
